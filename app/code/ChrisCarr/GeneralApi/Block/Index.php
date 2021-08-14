a13f9800a6ed97ad0a62ef20f847360000fb1092
chris carr <ccarr895@gmail.com>
2021-08-14 13:44:46 +0000

    frontend routing fixed

--- app/code/ChrisCarr/GeneralApi/Block/Index.php
+++ app/code/ChrisCarr/GeneralApi/Block/Index.php
@@ -1,31 +1,45 @@
 <?php
 namespace ChrisCarr\GeneralApi\Block;
 
-use Magento\Framework\HTTP\Client\Curl;
-use Magento\Framework\View\Element\Template;
-use Magento\Framework\View\Element\Template\Context;
-use ChrisCarr\GeneralApi\Helper\Data;
+use \Magento\Framework\HTTP\Client\Curl;
+use \Magento\Framework\View\Element\Template;
+use \Magento\Framework\View\Element\Template\Context;
+use \ChrisCarr\GeneralApi\Helper\Data;
 
 class Index extends Template
 {
-    protected $_curl;
 
-    protected $_helper;
+	protected $curl;
 
-    public function __construct(Context $context, Curl $curl, Data $data)
+	protected $helper;
+
+	public function __construct(
+		Context $context, 
+		Curl $curl, 
+		Data $hdata
+	)
     {
-        parent::__construct($context);
-        $this->_curl = $curl;
-        $this->_helper = $data;
+        $this->curl = $curl;
+	$this->helper = $hdata;
+	parent::__construct($context);
     }
 
     public function res()
     {
-        $this->_curl->addHeader(CURLINFO_CONTENT_TYPE, $this->_helper->getConfigFor("contentType"));
-        $this->_curl->addHeader("Content-Length", $this->_helper->getConfigFor("contentLength"));
-        $this->_curl->setOption(CURLOPT_RETURNTRANSFER, $this->_helper->getConfigFor("returnTransfer"));
-        $this->_curl->setOption(CURLOPT_PORT, $this->_helper->getConfigFor("port"));
-        $this->_curl->get($this->_helper->getConfigFor("url"));
-        return $this->_curl->getBody();
+        $this->curl->addHeader(CURLINFO_CONTENT_TYPE, $this->helper->getConfigFor("contentType"));
+        $this->curl->addHeader("Content-Length", $this->helper->getConfigFor("contentLength"));
+        $this->curl->setOption(CURLOPT_RETURNTRANSFER, $this->helper->getConfigFor("returnTransfer"));
+        $this->curl->setOption(CURLOPT_PORT, $this->helper->getConfigFor("port"));
+        $this->curl->get($this->helper->getConfigFor("url"));
+
+	$opts = [
+            "content-type" => $this->helper->getConfigFor("contentType"),
+            "content-length" => $this->helper->getConfigFor("contentLength"),
+            "returnTransfer" => $this->helper->getConfigFor("returnTransfer"),
+            "port" => $this->helper->getConfigFor("port"),
+            "url" => $this->helper->getConfigFor("url")
+        ];
+
+        return $opts;
     }
 }
