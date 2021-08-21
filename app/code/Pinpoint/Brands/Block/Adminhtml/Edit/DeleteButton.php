b50ce8140541000ba97f8baf04c580d82531a921
jynxmagic <ccarr895@gmail.com>
2021-08-21 22:52:47 +0100

    fixed some grid listing bugs

--- app/code/Pinpoint/Brands/Block/Adminhtml/Edit/DeleteButton.php
+++ app/code/Pinpoint/Brands/Block/Adminhtml/Edit/DeleteButton.php
@@ -2,18 +2,35 @@
 
 namespace Pinpoint\Brands\Block\Adminhtml\Edit;
 
+use Magento\Backend\Block\Widget\Context;
+use Magento\Framework\App\Request\Http;
 use Magento\Ui\Component\Control\Container;
+use Pinpoint\Brands\Api\BrandRepositoryInterface;
 
 class DeleteButton extends GenericButton
 {
+    /**
+     * @var Http $request
+     */
+    protected $request;
+
+    public function __construct(Context $context, BrandRepositoryInterface $brandRepository, Http $request)
+    {
+        parent::__construct($context, $brandRepository);
+        $this->request = $request;
+    }
+
     /**
      * @return array
      */
     public function getButtonData()
     {
+        if (!str_contains($this->request->getFullActionName(), "edit")) {
+            return [];
+        }
         return [
             'label' => __('Delete'),
-            'class' => 'delete',
+            'class' => 'primary delete',
             'data_attribute' => [
                 'mage-init' => [
                     'buttonAdapter' => [
@@ -29,9 +46,8 @@ class DeleteButton extends GenericButton
                     ]
                 ]
             ],
-            'class_name' => Container::SPLIT_BUTTON,
-            'options' => [],
-            'sort_order' => 90,
+            'class_name' => Container::DEFAULT_CONTROL,
+            'sort_order' => 90
         ];
     }
 }
